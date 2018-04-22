8c484c5ac3380ffd48714de4ea023b7526fcfc77
k3170makan <k3170makan@gmail.com>
2018-04-22 06:14:56 +0000

    otpionsparser,error loging

--- Cerebro.py
+++ Cerebro.py
@@ -3,6 +3,7 @@ import liblo
 import sys
 import time
 from random import random
+from optparse import OptionParser
 """
 	Cerebro - an actuation tool for eeg from the Muse head band data via pygame
 		tested and developed on a raspberry pi 3
@@ -24,6 +25,7 @@ from random import random
 						[?] - display thread checks an array to see if it must update the animation
 								array has a sequence number for each packet so it knows which one its displayed already?
 """	
+
 def eeg_callback(path, args,types,src,data):
 	global active
 	active = True	
@@ -33,18 +35,18 @@ def acc_callback(path,args,types,src,data):
 	global active
 	active = True	
 	log_args(args)
+
 def alpha_callback(path,args,types,src,data):
 	global active
 	global packet_index
 	packet_index += 1
 	active = True
-	sys.stderr.write("%d %s\n" % (packet_index,args[0]))
-	sys.stderr.flush()
+	print("%d %s" % (packet_index,args[0]))
 
 def log_args(args):
-	sys.stderr.write("\t%s  %s  %s  %s  %s\r" % (args[0],args[1],args[2],args[3],args[4]))
-	sys.stderr.flush()	
-
+	for arg in args:
+		print "\t%s " % (arg),
+	print
 #to flesh out later when I'm adding command line options
 def add_all(server):
 	return
@@ -55,11 +57,50 @@ def add_alpha_only(server):
 #Add other call backs	
 #TESTING CODE
 if __name__ == "__main__":
+	parser = OptionParser()
+	parser.add_option("--alpha-out",
+								dest="alpha_output_filename",\
+								type="string",\
+								action="store")
+
+	parser.add_option("--beta-out",\
+								dest="beta_output_filename",\
+								type="string",\
+								action="store")
+	
+	parser.add_option("--theta-out",\
+								dest="theta_output_filename",\
+								type="string",\
+								action="store")
+
+	parser.add_option("--delta-out",\
+								dest="delta_output_filename",\
+								type="string",\
+								action="store")
+
+	parser.add_option("--gamma-out",\
+								dest="gamma_output_filename",\
+								type="string",\
+								action="store")
+
+	parser.add_option("-p","--port",dest="port",action="store",type="int")
+	parser.add_option("-a","--alpha",dest="alpha",action="store_true")
+	parser.add_option("-b","--beta",dest="beta",action="store_true")
+	parser.add_option("-d","--delta",dest="delta",action="store_true")
+	parser.add_option("-t","--theta",dest="theta",action="store_true")
+	parser.add_option("-g","--gamma",dest="gamma",action="store_true")
+	options,args = parser.parse_args()
+
 	active = False
 	packet_index = 0
-	rand_port = 5000
-	print("[*] starting server on port %s" % (rand_port))
-	server = liblo.Server(rand_port)
+	if options.port != None:
+		server_port = options.port
+	else:
+		server_port = 5000
+	sys.stderr.write("[*] starting server on port %s\n" % (server_port))
+	sys.stderr.flush()
+
+	server = liblo.Server(server_port)
 	#server.add_method('/muse/eeg','fffff',eeg_callback,"EEG Service")
 	#server.add_method('/muse/acc','ffff',acc_callback,"ACC Service")
 	server.add_method('/muse/elements/alpha_absolute','f',alpha_callback,"Abs Alpha Service")
@@ -68,18 +109,30 @@ if __name__ == "__main__":
 	#server.add_method('/muse/elements/delta_absolute','f',delta_callback,"Abs Alpha Service")
 	#server.add_method('/muse/elements/gamma_absolute','f',gamma_callback,"Abs Alpha Service")
 	
-	print("[*] server started...")
+	sys.stderr.write("[*] server started...\n")
+	sys.stderr.flush()
 	event_index = 0
+	timing_queue = []
 	wait_start = time.time()
 	first_run = False
+	wait_index = 0
 	while True:
 		start = time.time()
-		ret = server.recv(128)		
+		ret = server.recv(64)		
 		if active:
 			if first_run:
 				wait_end = time.time()
 				first_run = True
-				print("[*] server got first packet in '%s' seconds!" % (wait_end - wait_start))
+				sys.stderr.write("[*] server got first packet in '%s' seconds!" % (wait_end - wait_start))
+				sys.stderr.flush()
 			end = time.time()
 			event_index+=1
-			print("%d %s" % (event_index,end - start))
+			timing_queue.append((end - start))
+			sys.stderr.write("\t\t\t%f %f %f\r" % ((end - start),sum(timing_queue),float(sum(timing_queue))/len(timing_queue)))
+			sys.stderr.flush()
+			if len(timing_queue) >= 100:
+				timing_queue = []
+		else:
+			sys.stderr.write("waiting for connection %s\r" % (\
+				["|","/","-","\\"][int(wait_index%4)]))
+			wait_index += 1
